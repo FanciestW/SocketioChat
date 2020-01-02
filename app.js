e9f71259576e16099f899a8ddc57ad60b63356fc
William Lin <wlin26@yahoo.com>
2020-01-01 21:49:37 -0500

    Added socket.io for simple connect and disconnect events

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     return res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connect', (socket) => {
+    console.log(`A User Connectioned with socket id: ${socket.id}`);
+    socket.on('disconnect', () => {
+        console.log(`A User has Disconnected with socket id: ${socket.id}`);
+    });
 });
 
 http.listen(PORT, () => {

7ac2b29febbd92c6b48dc0d49704e7a5398afb3d
William Lin <wlin26@yahoo.com>
2020-01-02 19:19:48 -0500

    Added socket.io logic to receive messages and welcome new users

--- app.js
+++ app.js
@@ -11,9 +11,18 @@ app.get('/', (req, res) => {
 io.on('connect', (socket) => {
     console.log(`User Connectioned with socket id: ${socket.id}`);
 
+    // Emit user connected message to all connected clients except for the sender client.
+    socket.broadcast.emit('user connected', { socketId: socket.id });
+
+    // Emit welcome event to the newly connected client socket.
+    socket.emit('welcome', { serverMsg: 'Welcome to the chat', });
+
     // When a chat message is received.
     socket.on('chat message', (msg) => {
         console.log(`User with socket id: ${socket.id} said: ${msg}`);
+
+        // Emit message from client to all other clients including the sender client.
+        io.emit('chat message', msg);
     });
 
     // When a socket/client disconnects.

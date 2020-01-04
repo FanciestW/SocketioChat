13592bb61cb2c5665784828b6f87039b37b44db9
William Lin <wlin26@yahoo.com>
2020-01-03 23:46:24 -0500

    Working on adding username support for each user

--- app.js
+++ app.js
@@ -11,18 +11,26 @@ app.get('/', (req, res) => {
 io.on('connect', (socket) => {
     console.log(`User Connectioned with socket id: ${socket.id}`);
 
+    socket.emit('ask name');
+    
     // Emit user connected message to all connected clients except for the sender client.
     socket.broadcast.emit('user connected', { socketId: socket.id });
 
-    // Emit welcome event to the newly connected client socket.
-    socket.emit('welcome', { serverMsg: 'Welcome to the chat', });
+    // When a new user joins and states username.
+    socket.on('new user', (username) => {
+
+        console.log(`New user named: ${username}`);
+        // Emit user connected message to all connected clients except for the sender client.
+        socket.broadcast.emit('user connected', { username, });
+        socket.emit('welcome', { serverMsg: `Welcome to the chat ${username}!`});
+    });
 
     // When a chat message is received.
     socket.on('chat message', (msg) => {
         console.log(`User with socket id: ${socket.id} said: ${msg}`);
 
         // Emit message from client to all other clients including the sender client.
-        io.emit('chat message', msg);
+        io.emit('chat message', { msg });
     });
 
     // When a socket/client disconnects.
